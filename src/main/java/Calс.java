029d04a9a1bb3bef7a1fb9f7b3987978789a0165
Denis Stepulenok <super.denis@gmail.com>
2015-02-01 14:08:47 +0300

    Реализация

---
+++ src/main/java/Calс.java
@@ -0,0 +1,124 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * Вычисление выражений
+ */
+public class Calс {
+    public static void main(String[] args) throws Exception {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        String foo = "40+2";
+        System.out.println(engine.eval(foo));
+    }
+
+    /**
+     * Вычисления при помощи JavaScript
+     *
+     * @param expression выражение
+     * @return результат
+     * @throws ScriptException
+     */
+    public static double calcJS(String expression) throws ScriptException {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        return Double.parseDouble(String.valueOf(engine.eval(expression)));
+    }
+
+    /**
+     * Вычисление
+     *
+     * @param str строка с выражением
+     * @return результат
+     */
+    public static double calc(final String str) {
+        class Parser {
+            int pos = -1, c;
+
+            void eatChar() {
+                c = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            void eatSpace() {
+                while (Character.isWhitespace(c)) eatChar();
+            }
+
+            double parse() {
+                eatChar();
+                double v = parseExpression();
+                if (c != -1) throw new RuntimeException("Unexpected: " + (char) c);
+                return v;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor | term brackets
+            // factor = brackets | number | factor `^` factor
+            // brackets = `(` expression `)`
+            double parseExpression() {
+                double v = parseTerm();
+                for (; ; ) {
+                    eatSpace();
+                    if (c == '+') { // addition
+                        eatChar();
+                        v += parseTerm();
+                    } else if (c == '-') { // subtraction
+                        eatChar();
+                        v -= parseTerm();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseTerm() {
+                double v = parseFactor();
+                for (; ; ) {
+                    eatSpace();
+                    if (c == '/') { // division
+                        eatChar();
+                        v /= parseFactor();
+                    } else if (c == '*' || c == '(') { // multiplication
+                        if (c == '*') eatChar();
+                        v *= parseFactor();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseFactor() {
+                double v;
+                boolean negate = false;
+                eatSpace();
+                if (c == '(') { // brackets
+                    eatChar();
+                    v = parseExpression();
+                    if (c == ')') eatChar();
+                } else { // numbers
+                    if (c == '+' || c == '-') { // unary plus & minus
+                        negate = c == '-';
+                        eatChar();
+                        eatSpace();
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    while ((c >= '0' && c <= '9') || c == '.') {
+                        sb.append((char) c);
+                        eatChar();
+                    }
+                    if (sb.length() == 0) throw new RuntimeException("Unexpected: " + (char) c);
+                    v = Double.parseDouble(sb.toString());
+                }
+                eatSpace();
+                if (c == '^') { // exponentiation
+                    eatChar();
+                    v = Math.pow(v, parseFactor());
+                }
+                if (negate) v = -v; // exponentiation has higher priority than unary minus: -3^2=-9
+                return v;
+            }
+        }
+        return new Parser().parse();
+    }
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import javax.script.ScriptException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тестирование вычислений
+ */
+public class CalcTest {
+
+    public static final double DELTA = 1e-10;
+
+    @Test
+    public void testCalcJS() throws ScriptException {
+        assertEquals(4.0, Calс.calcJS("2*2"), DELTA);
+        assertEquals(42.0, Calс.calcJS("40+2"), DELTA);
+        assertEquals(2.0 + 3.0 * 10, Calс.calcJS("2.0+3.0*10"), DELTA);
+        assertEquals(549.0, Calс.calcJS("2 + 7 * (9 * 9) - ((4 + 1) * (5 - 1))"), DELTA);
+        //assertEquals(sin(3), CalcJS.calcJS("sin(3)"), 1e-10);
+    }
+
+    @Test
+    public void testCalc() throws ScriptException {
+        assertEquals(4.0, Calс.calc("2*2"), DELTA);
+        assertEquals(42.0, Calс.calc("40+2"), DELTA);
+        assertEquals(2.0 + 3.0 * 10, Calс.calc("2.0+3.0*10"), DELTA);
+        assertEquals(549.0, Calс.calc("2 + 7 * (9 * 9) - ((4 + 1) * (5 - 1))"), DELTA);
+    }
+}
